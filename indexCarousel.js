d3226ed86539a4145f287344e8478eb29446c2cb
N <N>
2026-09-17 10:17:55 +0900

    updated ad logic to JSON

--- indexCarousel.js
+++ indexCarousel.js
@@ -2,21 +2,28 @@ window.onload = async () => {
     const $ = (e) => document.querySelector(e);
     const $$ = (e) => document.querySelectorAll(e);
 
-    const res = await fetch("/kkContents.html");
-    $(".kkBox").innerHTML = await res.text();
+    const res = await fetch("/kkContents.json");
+    const kkContents = await res.json();
 
     const todayStr = new Date().toLocaleDateString("sv-SE");
-    $$(".kkContent").forEach((e) => {
-        const from = e.getAttribute("from");
-        const to = e.getAttribute("to");
-        if (from && todayStr < from) {
-            e.remove();
-            return;
-        }
-        if (to && todayStr > to) {
-            e.remove();
-            return;
-        }
+
+    const kkContentsFiltered = kkContents.filter((e) => {
+        if (e.from && e.from > todayStr) return false; // 開始日より前
+        if (e.to && e.to < todayStr) return false; // 終了日より後
+        return true; // 表示対象
+    });
+
+    kkContentsFiltered.forEach((e) => {
+        const kkContentElem = document.createElement("div");
+        kkContentElem.className = "kkContent";
+        if (e.from) kkContentElem.setAttribute("from", e.from);
+        if (e.to) kkContentElem.setAttribute("to", e.to);
+        kkContentElem.innerHTML = `
+            <a target="_blank" href="${e.href}" data-ga4="${e.ga4}">
+                <img src="${e.img}" alt="${e.alt}" width="100%" />
+            </a>
+        `;
+        $(".kkContainer").appendChild(kkContentElem);
     });
 
     const containerElem = $(".kkContainer");
